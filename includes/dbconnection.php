8b716ec1c2c187cc0eaf4d4ada14e1da18d123c4
arnauvnci <87940547+arnauvnci@users.noreply.github.com>
2021-08-20 15:36:42 +0100

    no change as such

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -5,6 +5,7 @@ define('DB_USER','tiff1');
 define('DB_PASS','tiffinpassword1');
 define('DB_NAME','tiffin_db');
 // Establish database connection.
+//No changes made
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
